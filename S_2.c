5e3fbc730eddfb97c8cd062fc97933648a65305b
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-24 12:24:16 +1000

    Create S_2.c

---
+++ S_2.c
@@ -0,0 +1,17 @@
+Вы смотрели фильм «Матрица»? Если нет, то советую посмотреть. А задание будет следующим: переписать программу «Hello, world!» так, чтобы на экране появился следующий текст:
+
+Wake up, Neo...
+
+The Matrix has you...
+
+Follow the white rabbit...
+
+
+#include <stdio.h>
+
+int main() {
+  printf("Wake up, Neo...\n\n");
+  printf("The Matrix has you...\n\n");
+  printf("Follow the white rabbit...\n\n");
+  return 0;
+}
