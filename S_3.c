4684e4e362a66e542c3c1b97965c0e7fd851a413
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-24 12:28:33 +1000

    Create S_3.c

---
+++ S_3.c
@@ -0,0 +1,19 @@
+Напишите программу, которая выводит на экран следующие строки:
+
+NICKNAME: Neo
+CITY: St-Petersburg
+AGE: 35
+HEIGHT: 180
+WEIGHT: 75
+
+
+  #include <stdio.h>
+
+int main() {
+  printf("NICKNAME: Neo\n");
+  printf("CITY: St-Petersburg\n");
+  printf("AGE: 35\n");
+  printf("HEIGHT: 180\n");
+  printf("WEIGHT: 75\n");
+  return 0;
+}
