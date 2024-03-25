d202a67c1655a52b93d16d8124e91bdae49e1c1e
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-25 18:09:05 +1000

    Create S_6.c

---
+++ S_6.c
@@ -0,0 +1,14 @@
+Программа должна выводить на экран информацию о координатах его дома на Бейкер Стрит.
+
+S.Holmes:
+51 grad 31'25.48" N
+0 grad  9'29.93" W
+
+#include <stdio.h>
+
+int main(){
+    printf("S.Holmes:\n");
+    printf("51grad 31'25.48\" N\n");
+    printf("0 grad  9'29.93\" W\n");
+    return 0;
+}
