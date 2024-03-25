119528edec70605f784d0536b5855005b2a6f499
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-25 18:35:45 +1000

    Create S_7.c

---
+++ S_7.c
@@ -0,0 +1,11 @@
+Исправьте предложенную задачу так, чтобы она выводила следующий текст:
+
+"I'll be back!"
+        (c)Terminator
+
+#include <stdio.h>
+
+int main(){
+    printf("\"I'll be back!\"\n\t(c)Terminator");
+    return 0;
+}
