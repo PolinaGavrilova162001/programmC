2b939969220263c6256dabaee400a2e3c9360e37
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-27 18:54:03 +1000

    Update Laba6.c

--- Laba6.c
+++ Laba6.c
@@ -34,6 +34,41 @@ int main()
             }
         }
     }
+
+
+
+
+    #include <stdio.h>
+#include <stdlib.h>
+int main() {
+FILE *fpIn, *fpOut;
+char name[100], year[5];
+int yearRead;
+// Открываем входной и выходной файлы
+if ((fpIn = fopen("input.txt", "r")) == NULL) {
+    printf("Ошибка открытия входного файла!\n");
+    exit(1);
+}
+if ((fpOut = fopen("output.txt", "w")) == NULL) {
+    fclose(fpIn);
+    printf("Ошибка создания выходного файла!\n");
+    exit(2);
+}
+
+while (fscanf(fpIn, "%s %c", name, year) == 2) {
+    // Считываем данные из входного файла
+    yearRead = atoi(year);
+
+    if (yearRead > 1980) {
+        // Если год рождения больше 1980, записываем в выходной файл
+sprintf(year, "%d", yearRead); // форматированный вывод года
+fprintf(fpOut, "%s %s\n", name, year);
+}
+}
+fclose(fpIn);  // Закрываем входные файлы
+
+return 0;
+}
     fclose(input);
     fclose (output);
     
