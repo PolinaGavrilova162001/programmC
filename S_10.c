9720f3dde9d9a19d4a496d2c3ab8879879070d59
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-29 15:25:55 +1000

    Create S_10.c

---
+++ S_10.c
@@ -0,0 +1,20 @@
+В следующей программе переменные объявлены внутри программы. Исправьте её таким образом, чтобы значения переменных a, b, и с считывались функцией scanf.
+
+  #include <stdio.h> 
+int main(void) {
+  int a, b, res;
+  
+  scanf("%d", &a);
+    
+  scanf("%d", &b);
+
+  res = a + b;
+
+  int c;
+
+  scanf("%d", &c);
+  res = res - c;
+
+  printf("%d\n", res);
+  return 0;
+}
