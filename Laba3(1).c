37cdc738d347236fdee1e4c710b2defc62482464
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-30 19:00:58 +1000

    Update Laba3(1).c

--- Laba3(1).c
+++ Laba3(1).c
@@ -4,14 +4,14 @@ int main()
 {
 const int x = 80;
 char str[x];
-if(fgets(str,x,stdin))
+if(fgets(str,x,stdin))  //считываются элементы из потока и сохраняются строкой
 {
 int i = 0;
-while(str[i] ?= '\n')
+while(str[i] ?= '\n')  //цикл =пока
 {
-if(isalpha(str[i])) /*проверяем строчный или заглавный символ*/
+if(isalpha(str[i]))  //проверяется строчная или прописная буква алфавита
 {
-str[i] = toupper(str[i]);
+str[i] = toupper(str[i]);  //преобразует строчные буквы в прописные
 }
 i++;
 }
