c5cd6e904e40b4802b802ac911f2265d6660d9c1
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-30 19:24:59 +1000

    Update Laba6.c

--- Laba6.c
+++ Laba6.c
@@ -13,22 +13,22 @@ int main() {
     char first_name[20], middle_name[20], last_name[20];
     int year;
 
-    in_file = fopen("input.txt", "r");
-    out_file = fopen("output.txt", "w");
+    in_file = fopen("input.txt", "r");  //открывает файл для чтения
+    out_file = fopen("output.txt", "w");  //открывает файл и создает пустой файл для записи
 
     if (in_file == NULL || out_file == NULL) {
         printf("Error opening file\n");
-        return 1;
+        return 1;  //если файлы пустые, то выдает ошибку
     }
 
     while (fscanf(in_file, "%s %s %s %d", last_name, first_name, middle_name, &year) == 4) {
         if (year > 1980) {
-            fprintf(out_file, "%s %s %s %d\n", last_name, first_name, middle_name, year);
+            fprintf(out_file, "%s %s %s %d\n", last_name, first_name, middle_name, year);  //если файл заполнен, то читает его и записывает в выходной файл данные, которые удовлетворяют условию
         }
     }
 
     fclose(in_file);
-    fclose(out_file);
+    fclose(out_file);  //закрывает файлы
 
     return 0;
 }
