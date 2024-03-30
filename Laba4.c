323b5fa16d394886daf450ace10a141693007e33
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-30 19:06:52 +1000

    Update Laba4.c

--- Laba4.c
+++ Laba4.c
@@ -23,7 +23,7 @@ int main() {
     double a, b, c; printf("a b c: ");
 scanf("%lf%lf%lf", &a, &b, &c); 
 if (a <= 0 || b <= 0
-|| c <= 0 || a + b <= c || b + c <= a || c + a <= b) 
+|| c <= 0 || a + b <= c || b + c <= a || c + a <= b)  //|| - логическое или
 printf("Treygolnika net");
 else {
     perimetr(a, b, c); ploshchadj(a, b, c); 
