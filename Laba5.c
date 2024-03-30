324588d833b1300b93d7efb771aa2e2c85d92c8b
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-03-30 19:20:18 +1000

    Update Laba5.c

--- Laba5.c
+++ Laba5.c
@@ -6,12 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double **create_m(int n)
+double **create_m(int n)  //размерность массива
 {
-   double **m = malloc(sizeof(double*) * n);
+   double **m = malloc(sizeof(double*) * n);  //выделяет память под матрицу
    for (int i = 0; i < n; ++i)
    {
-      m[i] = calloc(1, sizeof(double) * n); 
+      m[i] = calloc(1, sizeof(double) * n);  //выделяет 1 байт на размерность матрицы
    }
    return m;
 }
@@ -20,41 +20,41 @@ void destroy_m(double **m, int n)
 { 
    for (int i = 0; i < n; ++i)
    { 
-      free(m[i]); 
+      free(m[i]);  //очистка памяти
    } 
    free(m); 
 }
 
 double** operation_m(double **m1, double **m2, char op, int n)
 {
-   switch (op)
+   switch (op)  //цикл для выбора
    {
-      case '+':
+      case '+':  //сложение матриц
          double **result = create_m(n);
          for (int i=0;i<n;++i)
             for (int j=0;j<n;++j)
                result[i][j] = m1[i][j] + m2[i][j];
                return result;
-      case '-':
+      case '-':  //вычитание матриц
          double **result2 = create_m(n);
          for (int i=0; i<n; ++i)
             for (int j=0; j<n; ++j)
             result2[i][j] = m1[i][j] - m2[i][j];
             return result2;
-      case '*':
+      case '*':  //умножение матриц
         double **result3 = create_m(n);
         for (int i=0; i<n; ++i)
             for (int j=0; j<n; ++j)
             result3[i][j] = m1[i][j] * m2[i][j];
             return result3;
-      case '/':
+      case '/':  //деление матриц
         double **result4 = create_m(n);
         for (int i=0; i<n; ++i)
             for (int j=0; j<n; ++j)
             result4[i][j] = m1[i][j]/m2[i][j];
             return result4;
             break;
-       default:
+       default:  //если операция не выбрана
         printf("Takou operacii net\n");
         break;
    }
@@ -62,18 +62,18 @@ double** operation_m(double **m1, double **m2, char op, int n)
 int main(void)
 {
 int n;
-printf("n=");
+printf("n=");  //запрос на ввод пользователя размерности матрицы
 scanf("%d", &n);
-double **m1 = create_m(n), **m2 = create_m(n);
+double **m1 = create_m(n), **m2 = create_m(n); 
     for(int i = 0; i < n; ++i)
         for(int j = 0; j < n; ++j)
-scanf("%lf", &m1[i][j]);
+scanf("%lf", &m1[i][j]);  //заполнение элементами первой матрицы
     for(int i = 0; i < n; ++i)
         for(int j = 0; j < n; ++j)
-scanf("%lf", &m2[i][j]);
+scanf("%lf", &m2[i][j]);  //заполнение элементами второй матрицы
 char op;
-printf("+, -, *, /: ");
-scanf(" %c", &op);
+printf("+, -, *, /: ");  //выбор операции над матрицами
+scanf(" %c", &op);  //ввод операции
 double** result = operation_m(m1, m2, op, n);
 for(int i = 0; i < n; ++i)
 {
