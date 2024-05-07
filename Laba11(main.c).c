d1f85d1729dff901c3a7c190c376a4434700fe94
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-05-07 18:25:45 +1000

    Create Laba11(main.c).c

---
+++ Laba11(main.c).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main()
+{
+    int sum = calculate_sum(3, 5, 10, 15);
+    int max = calculate_max(4, 20, 30, 15, 25);
+    int min = calculate_min(5, 10, 100, 30, 50, 20);
+    float average = calculate_average(4, 5, 10, 15, 20);
+    
+    printf("Sum: %d\n", sum);
+    printf("Max: %d\n", max);
+    printf("Min: %d\n", min);
+    printf("Average: %.2f\n", average);
+    
+    return 0;
+}
