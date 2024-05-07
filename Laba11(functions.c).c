8547772354c5a0f632678737b0cfbe17d258758f
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-05-07 18:27:14 +1000

    Create Laba11(functions.c).c

---
+++ Laba11(functions.c).c
@@ -0,0 +1,77 @@
+#include <stdarg.h>
+
+int calculate_sum(int num_args, ...)
+{
+    va_list args;
+    va_start(args, num_args);
+    
+    int sum = 0;
+    
+    for (int i =0; i < num_args; i++)
+    {
+        sum +=va_arg(args, int);
+    }
+    
+    va_end(args);
+    
+    return sum;
+}
+
+int calculate_max(int num_args, ...)
+{
+    va_list args;
+    va_start(args, num_args);
+    
+    int max = va_arg(args,int);
+    
+    for (int i = 0; i < num_args; i++)
+    {
+        int current = va_arg(args, int);
+        if (current > max)
+        {
+            max = current;
+        }
+    }
+    
+    va_end(args);
+    
+    return max;
+}
+
+int calculate_min(int num_args, ...)
+{
+    va_list args;
+    va_start(args, num_args);
+    
+    int min = va_arg(args, int);
+    
+    for (int i = 1; i < num_args; i++)
+    {
+        int current = va_arg(args, int);
+        if (current < min)
+        {
+            min = current;
+        }
+    }
+    
+    va_end(args);
+    
+    return min;
+}
+
+float calculate_average(int num_args, ...)
+{
+    va_list args;
+    va_start(args, num_args);
+    
+    int sum = 0;
+    
+    for (int i = 0; i < num_args; i++)
+    {
+        sum += va_arg(args, int);
+    }
+    
+    va_end(args);
+    
+    return (float) sum / num_args;
+}
