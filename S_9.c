e00e1328458f09cc5ecd55dc4479de885896904b
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-05-16 19:50:52 +1000

    Update S_9.c

--- S_9.c
+++ S_9.c
@@ -17,12 +17,13 @@ def
 
 
 
-#include <stdio.h> 
-int main(void){
+#include <stdio.h>  //подключаем основную библиотеку
 
-  printf("N\t10*N\t100*N\t1000*N\n\n");  
-  for (int i=1;i<=10;i++)
-    printf("%d\t%d\t%d\t%d\n",i,10*i,100*i,1000*i);
+int main(void)  //основная функция кода
+{
+  printf("N\t10*N\t100*N\t1000*N\n\n");  //вывод строки
+  for (int i=1;i<=10;i++)  //цикл
+    printf("%d\t%d\t%d\t%d\n",i,10*i,100*i,1000*i);  //вывод строки 
 
-  return(0);
+  return(0);  //конец функции
 }
