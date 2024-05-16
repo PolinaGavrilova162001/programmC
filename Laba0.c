fc58219e25aaff95740ce7773245957ff2caa11a
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-05-16 19:01:17 +1000

    Update Laba0.c

--- Laba0.c
+++ Laba0.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <stdio.h>    //подключение основной библиотеки C
 
-int main()
+int main()    //основная функция в коде C
 {
-    printf("Hello World");  //вывод текста
+    printf("Hello World");    //вывод текста
 
-    return 0;
+    return 0;    //конец функции main
 }
