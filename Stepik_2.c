250fce59a04d191cd34fa3069e9d71a32b51e6d7
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-05-16 19:37:58 +1000

    Update Stepik_2.c

--- Stepik_2.c
+++ Stepik_2.c
@@ -1,14 +1,14 @@
 Напишите программу, которая считает сумму двух целых чисел.
 
-#include <stdio.h>
+#include <stdio.h>  //подключаем основную библиотеку
 
-int main()
+int main()  //основная функция кода
 {
-int num1, num2, sum;
-scanf("%d", &num1);
-scanf("%d", &num2);
-sum = num1 + num2;
-printf("%d\n", sum);
+int num1, num2, sum;  //вводим переменные num1, num2, sum
+scanf("%d", &num1);  //считываем значение в переменную num1
+scanf("%d", &num2);  //считываем значение в переменную num2
+sum = num1 + num2;  //в переменную sum заносим значение суммы переменных num1 и num2
+printf("%d\n", sum);  //выводим значение переменной sum
 
-return 0;
+return 0;  //конец функции main
 }
