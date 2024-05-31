663d30c30829b8cc25eb252cf95e57fd8de0cce9
PolinaGavrilova162001 <159605017+PolinaGavrilova162001@users.noreply.github.com>
2024-05-31 10:05:03 +1000

    Update Create Laba11(functions.h).c

--- Create Laba11(functions.h).c
+++ Create Laba11(functions.h).c
@@ -1,9 +1,9 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-int calculate_sum(int num_args, ...);
-int calculate_max(int num_args, ...);
-int calculate_min(int num_args, ...);
-float calculate_average(int num_args, ...);
+int calculate_sum(int num_args, ...);  //функция для подсчета суммы
+int calculate_max(int num_args, ...);  //функция для подсчета максимального числа
+int calculate_min(int num_args, ...);  //функция для подсчета минимального числа
+float calculate_average(int num_args, ...);  //функция для подсчета среднего арифметического
 
 #endif
